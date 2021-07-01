31e9f5aed55c8da41204f5eea22987f25379f1b4
Gopinath V <gopinath.v.16@mountblue.tech>
2021-07-01 09:44:13 +0530

    authentication procedures changed

--- assignment/api.py
+++ assignment/api.py
@@ -3,16 +3,24 @@ from .serializers import AssignmentSerializer
 from .models import Assignment
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework import permissions
 from rest_framework.authentication import (TokenAuthentication,
                                            BasicAuthentication)
-from rest_framework.permissions import IsAuthenticated
 
 
 class AssignmentViewSet(viewsets.ModelViewSet):
     serializer_class = AssignmentSerializer
     queryset = Assignment.objects.all()
-    authentication_classes = (BasicAuthentication, TokenAuthentication, )
-    permission_classes = (IsAuthenticated, )
+    authentication_classes = [BasicAuthentication, TokenAuthentication]
+
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update', 'destroy', 'create']:
+            self.permission_classes = [permissions.IsAdminUser, ]
+        elif self.action in ['list']:
+            self.permission_classes = [permissions.IsAuthenticated, ]
+        else:
+            pass
+        return super(self.__class__, self).get_permissions()
 
     @action(methods=['get'], detail=False)
     def newest(self, request):
